1b37f15a37d80926d20040c7441a79746bd74bf0
alpnista06 <mariod.b.filho@gmail.com>
2018-05-25 13:07:22 -0300

    criando a biblioteca pwm

--- Firmware/main_irrigador/library_PWM.h
+++ Firmware/main_irrigador/library_PWM.h
@@ -21,25 +21,40 @@
 //Defines //
 ////////////
 
+//////////////////
+//Tipos de dados//
+/////////////////
+
+typedef enum {
+  PWM_prescaler_1
+  PWM_prescaler_8
+  WM_prescaler_64
+  _prescaler_1024
+}PWM_prescaler;
+
 //////////////////////////
 //Funções da Biblioteca //
 //////////////////////////
 
 /**
- * [configura_porta description]
- * @param u8_pino_adc [Configura a porta analógica à qual a leitura será realizada, default porta A0]
- * @param u8_referencia_adc [Configura a tensão de referencia para a conversão, default referencia interna]
- * @param u8_prescaler_adc [Configura o prescaler da conversão, default 128]
- */
-//void configura_PWM(uint8_t u8_pino_PWM, uint8_t u8_PWM_mode, uint8_t u8_prescaler_PWM);
+* [configura_porta description]
+* @param u8_pino_adc [Configura a porta analógica à qual a leitura será realizada, default porta A0]
+* @param u8_referencia_adc [Configura a tensão de referencia para a conversão, default referencia interna]
+* @param u8_prescaler_adc [Configura o prescaler da conversão, default 128]
+*/
+void configura_PWM(uint8_t u8_pino_PWM, PWM_prescaler prescaler_PWM);
+
+
 void debbuger_init_PWM();
 
 /**
- * [realizar_leitura analógica]
- * @param   [sem parametros de entrada]
- * @return          [retorna o resultado da conversão AD]
- */
-//void Pulso_PWM(uint8_t Valor);
+* [realizar_leitura analógica]
+* @param   [sem parametros de entrada]
+* @return          [retorna o resultado da conversão AD]
+*/
+void Pulso_PWM(uint8_t u8_pino_PWM, uint8_t Valor );
+
+
 void debbuger_write_PWM(uint16_t VALOR);
 
 #endif /*_LEITURA_ANALOGICA_H_*/
