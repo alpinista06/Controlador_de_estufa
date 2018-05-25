961ec5f1b4dcd2fe380879abff62d5ca74045ac3
alpnista06 <mariod.b.filho@gmail.com>
2018-05-25 15:45:27 -0300

    Vareao funcional da biblioteca PWM

--- Firmware/main_irrigador/library_PWM.h
+++ Firmware/main_irrigador/library_PWM.h
@@ -1,11 +1,11 @@
 /**
-* UNIVERSIDADE FEDERAL DE UBERLÂNDIA - UFU
-* Autor: Mario Divino Borges Filho
-* Data: 21/04/2018
-* Git: github.com/alpinista06
-*
-* Descrição: Biblioteca para escrita de PWM em bare metal
-* utilizando o microcontrolador ATMEGA328P
+  UNIVERSIDADE FEDERAL DE UBERLÂNDIA - UFU
+  Autor: Mario Divino Borges Filho
+  Data: 21/04/2018
+  Git: github.com/alpinista06
+
+  Descrição: Biblioteca para escrita de PWM em bare metal
+  utilizando o microcontrolador ATMEGA328P
 */
 #ifndef library_PWM_H_
 #define library_PWM_H_ //Previne quando multiplas inclusões
@@ -26,31 +26,34 @@
 /////////////////
 
 typedef enum {
-  PWM_prescaler_1
-  PWM_prescaler_8
-  WM_prescaler_64
-  _prescaler_1024
-}PWM_prescaler;
+  PWM_prescaler_1,
+  PWM_prescaler_8,
+  PWM_prescaler_64,
+  PWM_prescaler_256,
+  PWM_prescaler_1024
+} pwm_prescaler;
 
 //////////////////////////
 //Funções da Biblioteca //
 //////////////////////////
 
 /**
-* [configura_porta description]
-* @param u8_pino_adc [Configura a porta analógica à qual a leitura será realizada, default porta A0]
-* @param u8_referencia_adc [Configura a tensão de referencia para a conversão, default referencia interna]
-* @param u8_prescaler_adc [Configura o prescaler da conversão, default 128]
+  [configura_porta description]
+  @param u8_pino_adc [Configura a porta analógica à qual a leitura será realizada, default porta A0]
+  @param u8_referencia_adc [Configura a tensão de referencia para a conversão, default referencia interna]
+  @param u8_prescaler_adc [Configura o prescaler da conversão, default 128]
 */
-void configura_PWM(uint8_t u8_pino_PWM, PWM_prescaler prescaler_PWM);
+uint8_t configura_PWM(uint8_t u8_pino_PWM, pwm_prescaler prescaler_PWM);
 
 
 void debbuger_init_PWM();
 
+void init_PWM(uint8_t pino_pwm);
+
 /**
-* [realizar_leitura analógica]
-* @param   [sem parametros de entrada]
-* @return          [retorna o resultado da conversão AD]
+  [realizar_leitura analógica]
+  @param   [sem parametros de entrada]
+  @return          [retorna o resultado da conversão AD]
 */
 void Pulso_PWM(uint8_t u8_pino_PWM, uint8_t Valor );
 
